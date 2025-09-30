6f5a1908ad0f4dc49517a8d1f96b89bdebda24bb
JoaoWalter193 <joaowalter193@gmail.com>
2025-09-30 18:41:12 -0300

    pushar projeto com banco de dados

---
+++ app/src/main/java/com/example/walletapp/dao/TransactionDAO.kt
@@ -0,0 +1,79 @@
+package com.example.walletapp.dao
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import com.example.walletapp.data.model.Transaction
+import com.example.walletapp.data.db.DBHelper
+
+class TransactionDAO(private val context: Context) {
+
+    private val dbHelper = DBHelper(context)
+
+
+    //TEM QUE FAZER CREATE E READ E PESQUISA CUSTOM PARA BUSCAR SO CREDITO OU SO DEBITO
+
+   //CREATE
+   fun createTrasaction(transaction: Transaction): Long {
+       val db = dbHelper.writableDatabase
+       val values = ContentValues().apply {
+           put("tipo", transaction.tipo)
+           put("descricao", transaction.descricao)
+           put("valor", transaction.valor)
+       }
+       val id = db.insert(DBHelper.TABLE_NAME, null, values)
+       db.close()
+       return id
+   }
+
+
+    fun getAllTransactions(): List<Transaction> {
+        val db = dbHelper.readableDatabase;
+        val cursor: Cursor = db.query(DBHelper.TABLE_NAME, null,null,null,null,null,null)
+        val transactionList = mutableListOf<Transaction>()
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
+            val tipo = cursor.getString(cursor.getColumnIndexOrThrow("tipo"))
+            val descricao = cursor.getString(cursor.getColumnIndexOrThrow("descricao"))
+            val valor = cursor.getDouble(cursor.getColumnIndexOrThrow("valor"))
+            transactionList.add(Transaction(id,tipo,descricao,valor));
+        }
+        cursor.close()
+        db.close()
+        return transactionList
+    }
+
+    fun getDebitoTransactions(): List<Transaction>{
+        val db = dbHelper.readableDatabase;
+        val cursor: Cursor = db.query(DBHelper.TABLE_NAME, null,"tipo=debito",null,null,null,null)
+        val transactionList = mutableListOf<Transaction>()
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
+            val tipo = cursor.getString(cursor.getColumnIndexOrThrow("tipo"))
+            val descricao = cursor.getString(cursor.getColumnIndexOrThrow("descricao"))
+            val valor = cursor.getDouble(cursor.getColumnIndexOrThrow("valor"))
+            transactionList.add(Transaction(id,tipo,descricao,valor));
+        }
+        cursor.close()
+        db.close()
+        return transactionList
+    }
+
+    fun getCreditoTransactions(): List<Transaction>{
+        val db = dbHelper.readableDatabase;
+        val cursor: Cursor = db.query(DBHelper.TABLE_NAME, null,"tipo=credito",null,null,null,null)
+        val transactionList = mutableListOf<Transaction>()
+        while (cursor.moveToNext()){
+            val id = cursor.getInt(cursor.getColumnIndexOrThrow("id"))
+            val tipo = cursor.getString(cursor.getColumnIndexOrThrow("tipo"))
+            val descricao = cursor.getString(cursor.getColumnIndexOrThrow("descricao"))
+            val valor = cursor.getDouble(cursor.getColumnIndexOrThrow("valor"))
+            transactionList.add(Transaction(id,tipo,descricao,valor));
+        }
+        cursor.close()
+        db.close()
+        return transactionList
+    }
+
+
+}
---
+++ app/src/main/java/com/example/walletapp/data/model/Transaction.kt
@@ -0,0 +1,8 @@
+package com.example.walletapp.data.model
+
+data class Transaction(
+    val id: Int = 0,// incrementado dps
+    val tipo: String, // débito(gastos) e crédito(ganhos)
+    val descricao: String, // descricao doq é
+    val valor: Double // valor da operação
+)
