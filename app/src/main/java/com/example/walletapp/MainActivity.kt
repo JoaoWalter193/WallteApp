9bcb522494941c73c081b2eab69e2b12b93dc61c
Pedro Lemos <p.a.lemos1@gmail.com>
2025-09-30 20:13:03 -0300

    <feat> MainActivity & Botoes

--- app/src/main/java/com/example/walletapp/MainActivity.kt
+++ app/src/main/java/com/example/walletapp/MainActivity.kt
@@ -25,4 +25,13 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, CadastroActivity::class.java)
         startActivity(intent)
     }
+
+    fun telaExtrato(view: View){
+        val intent = Intent(this, CadastroActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun sair(view: View) {
+        finish()
+    }
 }
